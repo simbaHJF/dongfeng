b739fa8349e3b8bdcf5cfe883e5040b64fd78cdb
simba <simbahjf@gmail.com>
2019-10-24 17:26:54 +0800

    conflict fix

--- dongfeng-center/src/main/java/com/simba/dongfeng/center/service/impl/CenterExecutorServiceImpl.java
+++ dongfeng-center/src/main/java/com/simba/dongfeng/center/service/impl/CenterExecutorServiceImpl.java
@@ -5,6 +5,8 @@ import com.simba.dongfeng.center.dao.ExecutorDao;
 import com.simba.dongfeng.center.service.CenterExecutorService;
 import com.simba.dongfeng.center.pojo.ExecutorDto;
 import com.simba.dongfeng.common.pojo.ExecutorHeartbeatInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +19,7 @@ import javax.annotation.Resource;
  **/
 @Service
 public class CenterExecutorServiceImpl implements CenterExecutorService {
-
+    private Logger logger = LoggerFactory.getLogger(CenterExecutorServiceImpl.class);
     @Resource
     private ExecutorDao executorDao;
 
@@ -28,8 +30,10 @@ public class CenterExecutorServiceImpl implements CenterExecutorService {
         ExecutorDto heartbeatExecutorDto = PojoConverter.convertExecutorHeartbeatInfo(executorHeartbeatInfo);
         ExecutorDto executorDto = executorDao.selectExecutorByIp(heartbeatExecutorDto.getExecutorIp(), true);
         if (executorDto != null) {
+            logger.info("in update transactionManager,heartbeatExecutorDto:" + heartbeatExecutorDto);
             return executorDao.updateExecutor(heartbeatExecutorDto);
         } else {
+            logger.info("in insert transactionManager,heartbeatExecutorDto:" + heartbeatExecutorDto);
             return executorDao.insertExecutor(heartbeatExecutorDto);
         }
     }
